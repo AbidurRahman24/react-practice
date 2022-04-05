06b818a1f474fc919879457a8040f0e880c3a6eb
Niloy-Sumon <niloyabidur@gmail.com>
2022-04-06 02:30:08 +0600

    404 page

--- src/pages/NotFound/404.js
+++ src/pages/NotFound/404.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './404.css'
-import sadImo from './images/notfouldSadImogi.png'
+import sadImo from './images/notfound.svg'
 const NotFound = () => {
     return (
 
-        <div class="flex justify-center items-center h-screen text-center bg-slate-50">
+        <div class="flex justify-center items-center h-screen text-center bg-slate-50 ">
             <div className='space-y-4'>
-                <img className='text-slate-400 w-32 md:w-32 lg:w-48' style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }} src={sadImo} alt='sad Imogi' />
+                <img className='text-gray-500 w-28 md:w-32 lg:w-40 2xl:w-64' style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }} src={sadImo} alt='sad Imogi' />
                 <h2 className='text-5xl text-gray-500'> <strong>404</strong> </h2>
                 <h4 className='text-2xl text-gray-400'>Page not found</h4>
                 <div>
